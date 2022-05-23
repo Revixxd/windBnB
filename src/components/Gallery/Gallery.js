361b2c54bc8fd7b79e35e0ff54ec0533480bd558
Adam Myślicki <revixxdm@gmail.com>
2022-05-23 21:23:32 +0200

    adding props for article

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -2,17 +2,24 @@ import { GalleryStyled } from "./galleryStyling";
 
 import GalleryElement from "./GalleryElement"
 
+// importing data to show in gallery
+import data from "../../data/stays.json"
+
 export default function Gallery(){
+
+    const galleryElements = data.map(element =>{
+        return(
+
+            <GalleryElement {...element} />
+        )
+    })
+
+
     return(
         <GalleryStyled>
             <h1>Stays in LOCATION</h1>
             <div className="Gallerystyled--container">
-                <GalleryElement />
-                <GalleryElement />
-                <GalleryElement />
-                <GalleryElement />
-                <GalleryElement />
-                <GalleryElement />
+                {galleryElements}
             </div>
         </GalleryStyled>
     )
--- src/components/Gallery/GalleryElement.js
+++ src/components/Gallery/GalleryElement.js
@@ -1,23 +1,25 @@
 import {GalleryElementStyled} from "./galleryElementStyling"
-function GalleryElement(){
+function GalleryElement(props){
+    console.log(props)
+
     return(
         <GalleryElementStyled>
             <div className="imgDiv">
-                <img src="https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80" alt="location img"></img>
+                <img src={props.photo} alt="location img"></img>
             </div>
             <div className="infoDiv">
                 <div className="infoDivUp">
                     <div className="infoDivUp--hostApartmentDiv">
-                        <h5>Super Host</h5>
-                        <p>Private Room</p>
+                        {props.superHost && <h5>Super Host</h5> }
+                        <p>{props.type}</p>
                     </div>
                     <div className="infoDivUp--raitingDiv">
                         {/* star icon  */}
-                        <h6>4.40</h6>
+                        <h6>{props.rating}</h6>
                     </div>
                 </div>
                 <div className="infoDivDown">
-                    <h4>Description</h4>
+                    <h4>{props.title}</h4>
                 </div>
             </div>
         </GalleryElementStyled>
