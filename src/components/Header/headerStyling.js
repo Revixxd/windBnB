2471106e412cf7bff42c01b611130f1a4843528d
Adam Myślicki <revixxdm@gmail.com>
2022-05-30 10:53:45 +0200

    adding font for header

--- src/components/Header/headerStyling.js
+++ src/components/Header/headerStyling.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 export const HeaderStyled = styled.header`
+    font-family: 'Mulish', sans-serif;
     margin-top: 2vh;
     width: 100%;
     display: flex;
