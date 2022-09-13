640758063cdf1f2ef55f30cbafa80dbd65c74b7c
Adam Myślicki <revixxdm@gmail.com>
2022-09-13 16:48:16 +0200

    Update Search.js

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,122 +1,149 @@
-import React from "react";
-import SearchElementLocation from "./SearchElementLocation";
-import {SearchStyled} from "./searchStyling"
-import useWindowDimensions from "../windowSize"
+import React from 'react'
+import SearchElementLocation from './SearchElementLocation'
+import { SearchStyled } from './searchStyling'
+import useWindowDimensions from '../windowSize'
 
-function Search(props){
+function Search(props) {
+    const { width } = useWindowDimensions()
 
-    const { width,  } = useWindowDimensions();
-    
     const [isMobile, setIsMobile] = React.useState()
 
-    function increaseNumberOfGuests(){
-        if(props.numberOfGuests !== 10 ){
-
-            props.setNumberOfGueasts((prev)=> prev + 1)
-        }else{
+    function increaseNumberOfGuests() {
+        if (props.numberOfGuests !== 10) {
+            props.setNumberOfGueasts((prev) => prev + 1)
+        } else {
             return
         }
     }
-    function decreseNumberOfGuests(){
-        if(props.numberOfGuests > 0 ){
-
-            props.setNumberOfGueasts((prev)=> prev - 1)
-        }else{
+    function decreseNumberOfGuests() {
+        if (props.numberOfGuests > 0) {
+            props.setNumberOfGueasts((prev) => prev - 1)
+        } else {
             return
         }
     }
-
-    
 
     // make it seprate script or add it to widnowSize.js
-    React.useEffect( ()=>{
-        if(width <= 810){
+    React.useEffect(() => {
+        if (width <= 810) {
             setIsMobile(true)
-        }else{
+        } else {
             setIsMobile(false)
         }
     }, [width])
 
-
-    const elementsLocation = props.allCities.map(element =>{
-        return(
-
-            <SearchElementLocation handleClick = {props.setCity} content={element} currentCountry = {props.currentCountry}/>
+    const elementsLocation = props.allCities.map((element) => {
+        return (
+            <SearchElementLocation
+                handleClick={props.setCity}
+                content={element}
+                currentCountry={props.currentCountry}
+            />
         )
-        
     })
 
     const [selectedOption, setSelectedOption] = React.useState('')
 
-    function setSelected(parameter){
+    function setSelected(parameter) {
         setSelectedOption(parameter)
     }
 
-    return(
-
-
+    return (
         <SearchStyled>
-            
             <div className="container">
-                <div locaton onClick={() => setSelected("location")} className={`element element--boxShadow  ${selectedOption === "location" ? 'element--borderSelected' : ''}`}>
+                <div
+                    locaton
+                    onClick={() => setSelected('location')}
+                    className={`element element--boxShadow  ${
+                        selectedOption === 'location'
+                            ? 'element--borderSelected'
+                            : ''
+                    }`}
+                >
                     <h3>Location</h3>
                     <p>{props.currentSelectedCity}</p>
                 </div>
-                <div onClick={() => setSelected("guests")} className={`element element--boxShadow ${selectedOption === "guests" ? 'element--borderSelected' : ''}`}>
+                <div
+                    onClick={() => setSelected('guests')}
+                    className={`element element--boxShadow ${
+                        selectedOption === 'guests'
+                            ? 'element--borderSelected'
+                            : ''
+                    }`}
+                >
                     <h3>Guests</h3>
                     <p>Add Guests</p>
                 </div>
-                <div className={ `element element--button ${(isMobile ? '' : 'element--boxShadow')}`}>
-                    <button onClick = {props.handleClick} ><span  class="material-symbols-outlined">search</span>Search</button>
+                <div
+                    className={`element element--button ${
+                        isMobile ? '' : 'element--boxShadow'
+                    }`}
+                >
+                    <button onClick={props.handleClick}>
+                        <span class="material-symbols-outlined">search</span>
+                        Search
+                    </button>
                 </div>
-                
-                <div  className= {`chooseDiv ${selectedOption === "guests" ? `${isMobile ? "" : 'element--columnStart2'}` : ''}`}>
 
-                    {selectedOption === "location" && 
-                        <div className= "elementsLocationDiv">
+                <div
+                    className={`chooseDiv ${
+                        selectedOption === 'guests'
+                            ? `${isMobile ? '' : 'element--columnStart2'}`
+                            : ''
+                    }`}
+                >
+                    {selectedOption === 'location' && (
+                        <div className="elementsLocationDiv">
                             {elementsLocation}
                         </div>
-                    }
-                    
-                    {selectedOption === "guests" && 
-                        <div className="chooseDiv--selectedOption" >
+                    )}
+
+                    {selectedOption === 'guests' && (
+                        <div className="chooseDiv--selectedOption">
                             <div className="chooseDiv__selectedOption--element">
                                 <div className="chooseDiv__selectedOption__element--textDiv">
-
                                     <h4>Adults</h4>
                                     <p>Ages 13 or above</p>
-                                    
                                 </div>
 
                                 <div className="chooseDiv__selectedOption__element--inputDiv">
-                                    <button onClick={decreseNumberOfGuests}><span class="material-symbols-outlined">remove</span></button>
+                                    <button onClick={decreseNumberOfGuests}>
+                                        <span class="material-symbols-outlined">
+                                            remove
+                                        </span>
+                                    </button>
                                     <p>{props.numberOfGuests}</p>
-                                    <button onClick={increaseNumberOfGuests}><span class="material-symbols-outlined">add</span></button>
+                                    <button onClick={increaseNumberOfGuests}>
+                                        <span class="material-symbols-outlined">
+                                            add
+                                        </span>
+                                    </button>
                                 </div>
                             </div>
                             <div className="chooseDiv__selectedOption--element">
                                 <div className="chooseDiv__selectedOption__element--textDiv">
-
                                     <h4>Children</h4>
                                     <p>Ages 2-12</p>
-                                    
                                 </div>
 
                                 <div className="chooseDiv__selectedOption__element--inputDiv">
-                                    <button onClick={decreseNumberOfGuests}><span class="material-symbols-outlined">remove</span></button>
+                                    <button onClick={decreseNumberOfGuests}>
+                                        <span class="material-symbols-outlined">
+                                            remove
+                                        </span>
+                                    </button>
                                     <p>{props.numberOfGuests}</p>
-                                    <button onClick={increaseNumberOfGuests}><span class="material-symbols-outlined">add</span></button>
+                                    <button onClick={increaseNumberOfGuests}>
+                                        <span class="material-symbols-outlined">
+                                            add
+                                        </span>
+                                    </button>
                                 </div>
                             </div>
                         </div>
-                    }
-
+                    )}
                 </div>
-                
             </div>
-
-            
-
         </SearchStyled>
     )
 }
